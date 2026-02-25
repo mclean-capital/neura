15e0a12aeeee91dada94e9df8bb1e0d3193c269c
Don Mclean <donmcleanx@gmail.com>
2026-02-24 20:56:33 -0500

    fresh start

--- backend/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import tokenRouter from "./routers/token.js";
-
-const apiRouterV3 = express.Router();
-
-apiRouterV3.use("/token", tokenRouter);
-
-export default apiRouterV3;
